57c372230faeb159d772845ed6cf3affc894e250
Aaron Huggins <ahuggins@nethealthshops.com>
2020-08-26 12:03:48 -0500

    Clean up more magic values

--- lib/strongType.ts
+++ lib/strongType.ts
@@ -1,4 +1,5 @@
 import { isType, constrainTypes, looseType } from './typeOf'
+import { TAsyncFunction, TFunction } from './types'
 import type { CustomType, Type } from './types'
 
 type ArgumentLike<T> = Array<T>
@@ -11,11 +12,11 @@ type FunctionLike<A, T> = (...args: ArgumentLike<A>) => T
 export function StrongFunction<T>(types: Array<Type | Type[] | CustomType<string> | CustomType<string>[]>, fn: FunctionLike<any, T>): FunctionLike<any, T>
 export function StrongFunction<T>(types: Array<Type | Type[] | CustomType<string> | CustomType<string>[]>, fn: AsyncFunctionLike< any, T>): AsyncFunctionLike<any, T>
 export function StrongFunction<T>(types: Array<Type | Type[] | CustomType<string> | CustomType<string>[]>, fn: FunctionLike<any, T> | AsyncFunctionLike< any, T>): FunctionLike<any, T> | AsyncFunctionLike<any, T> {
-  if (!isType(fn, 'function', 'asyncfunction')) throw new TypeError('Argument fn is required and must be a function.')
+  if (!isType(fn, TFunction, TAsyncFunction)) throw new TypeError('Argument fn is required and must be a function.')
   const self = this
   let func: FunctionLike<any, T> | AsyncFunctionLike<any, T>
 
-  if (isType(fn, 'asyncfunction')) {
+  if (isType(fn, TAsyncFunction)) {
     func = async function (...args: any[]) {
       const error = constrainTypes(types, ...args)
 
@@ -44,11 +45,11 @@ export function StrongFunction<T>(types: Array<Type | Type[] | CustomType<string
 export function WeakFunction<A, T>(type: Type | Type[] | CustomType<string> | CustomType<string>[], fn: FunctionLike<A, T>): FunctionLike<A, T>
 export function WeakFunction<A, T>(type: Type | Type[] | CustomType<string> | CustomType<string>[], fn: AsyncFunctionLike<A, T>): AsyncFunctionLike<A, T>
 export function WeakFunction<A, T>(type: Type | Type[] | CustomType<string> | CustomType<string>[], fn: FunctionLike<A, T> | AsyncFunctionLike<A, T>): FunctionLike<A, T> | AsyncFunctionLike<A, T> {
-  if (!isType(fn, 'function', 'asyncfunction')) throw new TypeError('Argument fn is required and must be a function.')
+  if (!isType(fn, TFunction, TAsyncFunction)) throw new TypeError('Argument fn is required and must be a function.')
   const self = this
   let func: FunctionLike<A, T> | AsyncFunctionLike<A, T>
 
-  if (isType(fn, 'asyncfunction')) {
+  if (isType(fn, TAsyncFunction)) {
     func = async function (...args: any[]) {
       const error = looseType(type, ...args)
 
